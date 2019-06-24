287524c60fc9228dca63a45c3371726f152d3fc1
KyleCole01 <kyle@misfitselectronics.com>
2019-06-24 16:14:38 -0400

    debug me :) 2

--- kotlinstory/src/main/kotlin/com/example/kotlinstory/repository/StoryRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kotlinstory.repository
-
-import com.example.kotlinstory.models.Story
-import org.springframework.data.repository.CrudRepository
-interface StoryRepository : CrudRepository<Story, Long>
